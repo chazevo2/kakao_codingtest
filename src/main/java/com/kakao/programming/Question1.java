5ff0e98f6362dbf7ecc18aa7f9523ea96232cb62
chazevo2 <chazevo2@gmail.com>
2019-09-08 05:15:38 +0900

    add Question1 solution

--- src/main/java/com/kakao/programming/Question1.java
+++ src/main/java/com/kakao/programming/Question1.java
@@ -4,19 +4,28 @@ public class Question1 {
 	public int solution(String s) {
 		int answer = 0;
 		StringBuilder sb = new StringBuilder();
-		
+
 		char[] c = s.toCharArray();
-		int n = 0;
-		for (int i = 0; i < c.length; i++) {
-			if(c[i] == c[i+1]) {
-				n++;
+		int count = 1;
+		for (int i = 1; i < c.length; i++) {
+			if (c[i - 1] == c[i]) {
+				count++;
 			} else {
-				sb.append(n);
-				sb.append(c[i]);
-				n = 0;
+				if (count != 1)
+					sb.append(count);
+				sb.append(c[i - 1]);
+				count = 1;
 			}
+			
+			if(i == c.length - 1) {
+				if (count != 1)
+					sb.append(count);
+				sb.append(c[i]);
+			}	
 		}
-		
+
+		System.out.println(sb);
+		answer = sb.length();
 		return answer;
 	}
 }
